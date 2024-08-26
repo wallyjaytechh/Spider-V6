788d730b8a65a11bae49f80fbae4f9567ea8ac50
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2024-08-26 11:02:54 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,25 +2,25 @@
 
 
 require("./all/module")
-global.owner = "254742491666" 
-global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
-global.autoJoin = false 
-global.antilink = false 
+global.owner = "2348144317152" 
+global.namaCreator = "ANONYMOUS" 
+global.autoJoin = true 
+global.antilink = true 
 global.versisc = '6.0' 
-global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
-global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
+global.codeInvite = "https://wa.me/2348144317152"
+global.isLink = 'https://wa.me/2348144317152'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
-global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.packname = "ANONYMOUS-BUGGER" 
+global.author = "ANONYMOUS" 
 global.jumlah = "5" 
-global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
-global.domain = 'https://kingsam.sellerpanelerol.me' // your domian
+global.namabot = "ANONYMOUS-MD" 
+global.domain = 'https://wa.me/2348144317152' // your domian
 global.apikey = 'ptla_VRxkhvalZQ3EhHyAax9QYxgT5cbsx2MrO69gBpVZmmd' // Isi Apikey Plta Lu
 global.capikey = 'ptlc_sQ3d4GxCiztyV5DbhUJSWtvnSCyKzeMXoWIr2lVyqal' // Isi Apikey Pltc Lu
 global.eggsnya = '15' // id eggs yang dipakai
-global.location = '1' // id location
+global.location = '234' // id location
 global.apilinode = ''
 global.apitokendo = ''
 let file = require.resolve(__filename)
